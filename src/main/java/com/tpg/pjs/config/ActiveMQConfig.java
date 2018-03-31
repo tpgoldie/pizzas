030db4a36ad0d27a9d0d67a09ff6deda38fd99c1
Tony Golding <tpg@blueyonder.co.uk>
2018-03-31 22:33:10 +0100

    Added active mq config

--- src/main/java/com/tpg/pjs/config/ActiveMQConfig.java
+++ src/main/java/com/tpg/pjs/config/ActiveMQConfig.java
@@ -1,19 +1,40 @@
 package com.tpg.pjs.config;
 
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+import javax.jms.ConnectionFactory;
 
 @EnableJms
 @Configuration
 public class ActiveMQConfig {
 
     @Bean
-    public JmsListenerContainerFactory<?> queueListenerFactory() {
+    public JmsListenerContainerFactory<?> listenerContainerFactory(ConnectionFactory connectionFactory,
+                                                    DefaultJmsListenerContainerFactoryConfigurer configurer) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
 
-        return new DefaultJmsListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+
+        return factory;
+    }
+
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+
+        return converter;
     }
 
     public static final String ORDER_QUEUE = "orders-queue";
